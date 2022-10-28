dde7a5c1fb8ce30fb604a65da4e1cbfc51ca5c11
timothy <timothybrennan8@gmail.com>
2022-10-28 15:17:21 -0400

    apparently we dont need ssl

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 const { Client } = require('pg'); 
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
+const client = new Client(connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+  
 
 
 async function getAllUsers() {
