28f5f387144bde6d0d076474671f4a816ec785f3
timothy <timothybrennan8@gmail.com>
2022-10-28 11:59:48 -0400

    added the registerUser function and tested with token

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const usersRouter = express.Router();
 const jwt = require('jsonwebtoken');
 
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
 
 usersRouter.use((req, res, next) => {
     console.log("A request is being made to /users");
@@ -43,6 +43,42 @@ usersRouter.post('/login', async (req, res, next) => {
     } catch (error) {
         console.log(error);
         next(error);
+    }
+});
+
+usersRouter.post('/register', async (req, res, next) => {
+    const {username, password, name, location} = req.body;
+
+    try {
+        const _user = await getUserByUsername(username);
+
+        if (_user) {
+            next ({
+                name: 'UserExistsError',
+                message: 'a user by that username already exists'
+            });
+        }
+
+        const user = await createUser({
+            username,
+            password,
+            name,
+            location,
+        });
+
+        const token = jwt.sign({
+            id: user.id,
+            username
+        }, process.env.JWT_SECRET, {
+            expiresIn: '1w'
+        });
+
+        res.send({
+            message: "thank you for signing up",
+            token
+        });
+    } catch ({ name, message}) {
+        next({ name, message})
     }
 });
 
